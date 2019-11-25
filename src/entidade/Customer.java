083856fdbe994d8e9ebb8de3ddae8f8bac2a3aa6
michel robaskiewicz <michel robaskiewicz>
2019-11-25 20:00:46 -0300

    Refactoring 3 Move Method

--- src/entidade/Customer.java
+++ src/entidade/Customer.java
@@ -28,6 +28,10 @@ class Customer {
     public String getName() {
         return _name;
     }
+    
+    private double amountFor(Rental aRental) {
+      return aRental.getCharge();  
+   }
 
     public String statement() {
         double totalAmount = 0;
@@ -37,9 +41,7 @@ class Customer {
         while (rentals.hasMoreElements()) {
             double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
-            
-            thisAmount = amountFor(each);
-
+            thisAmount = each.getCharge();
             //determine amounts for each line
             // add frequent renter points
             frequentRenterPoints++;
@@ -61,26 +63,8 @@ class Customer {
                 + " frequent renter points";
         return result;
     }
+    
+    
 
-    private double amountFor(Rental aRental) {
-        double result = 0;
-        switch (aRental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (aRental.getDaysRented() > 2) {
-                    result += (aRental.getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                result += aRental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (aRental.getDaysRented() > 3) {
-                    result += (aRental.getDaysRented() - 3) * 1.5;
-                }
-                break;
-        }
-        return result;
-    }
+    
 }
